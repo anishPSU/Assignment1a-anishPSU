21778bab816a6bd8700d416929ff6c2042f1de45
Anish Harigopal <Axh5984@psu.edu>
2024-09-12 16:29:11 -0400

    Your message

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "anishPSU";
+    char STUDENT_ID[] = "940159235";
 
     return 0 ;
 }
+
